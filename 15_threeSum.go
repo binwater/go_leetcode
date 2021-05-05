771998e9f175589986313354cf2b0be305a2cea6
binwater <2329088424@qq.com>
2021-05-05 22:52:31 +0800

    add two code

--- 15_threeSum.go
+++ 15_threeSum.go
@@ -34,7 +34,7 @@ import (
 //链接：https://leetcode-cn.com/problems/3sum
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-func threeSum(nums []int) [][]int {
+func threeSum1(nums []int) [][]int {
 	sort.Ints(nums)
 	result := [][]int{}
 	for i := 0; i < len(nums)-1; i++ {
@@ -72,6 +72,43 @@ func threeSum(nums []int) [][]int {
 	return result
 }
 
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	result := make([][]int, 0)
+
+	for i:=0; i<len(nums)-1; i++{
+		if i>0 && nums[i] == nums[i-1]{
+			continue
+		}
+
+		j := i+1
+		z := len(nums)-1
+		for j < z{
+			b := nums[j]
+			c := nums[z]
+			if nums[i] + b +c > 0 {
+				z--
+			} else if nums[i] + b +c < 0 {
+				j++
+			} else {
+				tmp := []int{nums[i], b, c}
+				result = append(result, tmp)
+
+				for j < z && nums[j] == nums[j+1]{
+					j++
+				}
+				for j < z && nums[z] == nums[z-1]{
+					z--
+				}
+				j++
+				z--
+			}
+		}
+	}
+
+	return result
+}
+
 func main() {
 	nums := []int{-1,0,1,2,-1,-4}
 	fmt.Println(threeSum(nums))
@@ -81,4 +118,8 @@ func main() {
 
 	nums = []int{0}
 	fmt.Println(threeSum(nums))
+
+	nums = []int{1,-1,-1,0}
+	fmt.Println(threeSum(nums))
+
 }
